e309edcdd3cc1d5111a567dba54bc9810de33ca7
Uriel <ur.sabatista@gmail.com>
2021-01-10 17:44:10 -0300

    Adicionando porta como variavel de ambiente

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var postMessageRoutes = require('./controllers/postMessageController')
 var app = express()
 app.use(bodyParser.json())
 app.use(cors())
-app.listen(4000,()=>console.log('Server iniciou na porta : 4000'))
+app.listen(process.env.PORT || 4000,()=>console.log('Server iniciou na porta : 4000'))
 
 
 app.use('/postMessages',postMessageRoutes)
